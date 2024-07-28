fd9838646e1b8dc04092aae3cf453f4329abe43c
Joseph Ampadu <jkampadujnr@gmail.com>
2024-07-28 14:04:39 +0000

    Create CodeforcesTemplate.java

---
+++ CodeforcesTemplate.java
@@ -0,0 +1,69 @@
+// ░▒▓██████▓▒░░▒▓█████▓▒░▒▓██████▓▒░▒▓█▓▒░▒▓█▓▒░▒▓█████▓▒░▒▓█████████████▓▒░
+// ░▒▓█▓▒░▒▓█▓▒▒▓█▓▒░▒▓█▓▒▒▓█▓▒░▒▓█▓▒▒▓█▓▒░▒▓█▓▒▒▓█▓▒░▒▓█▓▒▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░
+// ░▒▓█▓▒░▒▓█▓▒▒▓█▓▒░▒▓█▓▒▒▓█▓▒░▒▓█▓▒▒▓█▓▒░▒▓█▓▒▒▓█▓▒░▒▓█▓▒▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░
+// ░▒▓█▓▒░▒▓█▓▒▒▓███████▓▒▒▓█▓▒░▒▓█▓▒▒▓█▓▒░▒▓█▓▒▒▓███████▓▒▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░
+// ░▒▓█▓▒░▒▓█▓▒▒▓█▓▒░▒▓█▓▒▒▓█▓▒░▒▓█▓▒▒▓█▓▒░▒▓█▓▒▒▓█▓▒░▒▓█▓▒▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░
+// ░▒▓█▓▒░▒▓█▓▒▒▓█▓▒░▒▓█▓▒▒▓█▓▒░▒▓█▓▒▒▓█▓▒░▒▓█▓▒▒▓█▓▒░▒▓█▓▒▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░
+// ░▒▓██████▓▒░▒▓█▓▒░▒▓█▓▒▒▓██████▓▒░░▒▓█████▓▒░▒▓█▓▒░▒▓█▓▒▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+/**
+ * Codeforces Round <round> (Div. <division>)
+ * <problem>. <problem name>
+ */
+public class CodeforcesTemplate {
+    public static void main(String[] args) {
+        Kattio io = new Kattio();
+
+        int t = io.nextInt();
+
+        while (t-- > 0) {
+            int n = io.nextInt();
+        }
+
+        io.close();
+    }
+
+    static class Kattio extends PrintWriter {
+        private BufferedReader r;
+        private StringTokenizer st;
+
+        public Kattio() {
+            this(System.in, System.out);
+        }
+
+        public Kattio(InputStream i, OutputStream o) {
+            super(o);
+            r = new BufferedReader(new InputStreamReader(i));
+        }
+
+        public String next() {
+            try {
+                while (st == null || !st.hasMoreTokens()) {
+                    st = new StringTokenizer(r.readLine());
+                }
+                return st.nextToken();
+            } catch (Exception e) {
+            }
+            return null;
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
